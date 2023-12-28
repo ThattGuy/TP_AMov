ac646cd821a066653e02911ee087f129ed39d1be
diogocsgomes <diogocsgomes@gmail.com>
2023-12-28 00:36:36 +0000

    registo de ategoria na BD implementado

--- app/src/main/java/pt/isec/amov/tp/eguide/ui/screens/RegisterCategory.kt
+++ app/src/main/java/pt/isec/amov/tp/eguide/ui/screens/RegisterCategory.kt
@@ -1,5 +1,7 @@
 package pt.isec.amov.tp.eguide.ui.screens
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
@@ -11,15 +13,41 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 @Composable
 fun RegisterCategory(){
+
+
+
+
+    val db = Firebase.firestore
+
+
+
     Column(verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally) {
 
-        var name by remember { mutableStateOf("")}
-        TextField(value = name, onValueChange = { name = it } , label = {Text("Nome da categoria")})
-        Button(onClick = { /*TODO*/ }) {
+        var categoryName by remember { mutableStateOf("")}
+        var categoryDescription by remember { mutableStateOf("")}
+        TextField(value = categoryName, onValueChange = { categoryName = it } , label = {Text("Nome da categoria")})
+        TextField(value = categoryDescription, onValueChange = { categoryDescription = it } , label = {Text("Descrição da categoria")})
+        Button(onClick = {
+            print("\n\n\n" + categoryName)
+            val nameData = hashMapOf("Name" to categoryName, "Description" to categoryDescription)
+
+
+            db.collection("Categories").document(categoryName).set(nameData)
+                .addOnSuccessListener {
+                    Log.i(TAG, "addDataToFirestore: Success")
+                }
+                .addOnFailureListener { e->
+                    Log.i(TAG, "addDataToFirestore: ${e.message}")
+                }
+
+
+        }) {
             Text(text = "Save")
         }
     }
